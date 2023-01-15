37f5e1edb16ae1a6dd2f7a378fe2bcfc31c26e86
seonghwa <ghdtack@ajou.ac.kr>
2023-01-15 16:46:36 +0900

    230115 #6.4 cleanup function 끝 <- 잘 쓰지 않는다. / 이론 끝

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,14 +1,26 @@
-import Button from "./Button"
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
+function Hello(){
+  function byFn(){
+    console.log("bye :(");
+  }
+  function hiFn(){
+    console.log("created :)");
+    return byFn;
+  }
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
 
 
 function App() {
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
+  const onClickShowing = () => setShowing((prev) => !prev);
   useEffect(() => {
     console.log("I run only once.");
   }, []);
@@ -32,6 +44,8 @@ function App() {
         {counter}
       </h1>
       <button onClick={onClick}>click me</button>
+      <button onClick={onClickShowing}>{showing ? "Hide" : "Show"}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
