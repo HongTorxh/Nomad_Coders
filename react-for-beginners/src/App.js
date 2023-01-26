586464fc01a738874632c7d2f76baca9d7ba27b4
seonghwa <ghdtack@ajou.ac.kr>
2023-01-26 15:59:13 +0900

    #7.4 movie app 만들기 / 현재까지 배운 useState, effect, map 등 복습 및 async & await를 배우는 시간

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,92 +1,40 @@
-// import { useState } from "react";
-
-// function App() {
-//   const [toDo, setToDo] = useState("");
-//   const [toDos, setToDos] = useState([]);
-//   const onChange = (event) => setToDo(event.target.value);
-//   const onSubmit = (event) => {
-//     event.preventDefault();
-//     if (toDo === "") {
-//       return;
-//     }
-//     setToDos((currentArray) => [toDo, ...currentArray]);
-//     setToDo("");
-//   };
-//   return (
-//     <div>
-//       <h1>My To ToDos ({toDos.length})</h1>
-//       <form onSubmit={onSubmit}>
-//         <input
-//           onChange={onChange}
-//           value={toDo}
-//           type="text"
-//           placeholder="Write your to do..."
-//         />
-//         <button> Add To Do</button>
-//       </form>
-//       <hr />
-//       <ul>
-//         {toDos.map((item, index) => (
-//           <li key={index}>{item}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import { useEffect, useState } from "react";
-
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [cost, setCost] = useState(1);
-  const [need, setNeed] = useState(1);
-  const onChange = (event) => {
-    setCost(event.target.value);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
-  const handleInput = (event) => {
-    console.log(event);
-    setNeed(event.target.value);
-  };
-
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
   return (
     <div>
-      <h1>The Coins! ({coins.length})</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1> Loading...</h1>
       ) : (
-        <select onChange={onChange}>
-          {coins.map((coin, index) => (
-            <option
-              key={index}
-              value={coin.quotes.USD.price}
-              id={coin.symbol}
-              symbol={coin.symbol}
-            >
-              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-            </option>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <a href={movie.url}>{movie.url}</a>
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
           ))}
-        </select>
+        </div>
       )}
-      <div>
-        <input
-          type="number"
-          value={need}
-          onChange={handleInput}
-          placeholde="dollar"
-        ></input>
-        <p> 달러당 비트코인 : {need / cost}</p>
-      </div>
     </div>
   );
 }
